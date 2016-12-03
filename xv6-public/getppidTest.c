9740d2aa78a744cbbcdc4bdbac815bb5b9040066
hamid3potter <hamid3potter@gmail.com>
2016-12-03 20:39:30 +0330

    a little bug fixed

--- xv6-public/getppidTest.c
+++ xv6-public/getppidTest.c
@@ -5,6 +5,8 @@
 int
 main (void)
 {
- printf(1,"this is a command for testing new method added!\n");
+ printf(1,"**************************************************\n");
  printf(1, "the parent process id of current function is : %d\n" );
+ printf(1,"**************************************************\n");
+ exit();
 }
