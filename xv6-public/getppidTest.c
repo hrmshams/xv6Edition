62dfdac59a7af4a078c578c05610850992be5b40
hamid3potter <hamid3potter@gmail.com>
2016-12-03 20:25:29 +0330

    new system call has been added!

---
+++ xv6-public/getppidTest.c
@@ -0,0 +1,10 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main (void)
+{
+ printf(1,"this is a command for testing new method added!\n");
+ printf(1, "the parent process id of current function is : %d\n" );
+}
